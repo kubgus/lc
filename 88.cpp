c52c675e2f7458a58e956d37779924f49f08dc11
Jakub G <kubgus@latitude.home>
2024-01-16 07:15:00 -0500

    Merge Sorted Array (improved)

--- 88.cpp
+++ 88.cpp
@@ -8,19 +8,16 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int l = 0, r = 0;
-        if (m == 0) {
-            nums1 = nums2;
-            return;
-        }
-        if (n == 0) return;
-        while (l < m + n) {
-            if (r == nums2.size()) return;
-            if (nums2[r] < nums1[l] || (r < n && l >= m)) {
-                nums1.insert(nums1.begin() + l, nums2[r]);
-                nums1.pop_back();
-                l++; r++; m++;
-            } else l++;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
         }
     }
 };
